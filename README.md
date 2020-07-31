d6a14967d4772f447eb10932ce20fb60bca96196
Raphael Discky <zundria.putra@gmail.com>
2020-07-31 16:47:36 +0700

    Change md5 to bcrypt

---
+++ README.md
@@ -0,0 +1,1 @@
+Authentication and security website practice 
--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -33,32 +34,37 @@ app.get("/register", function(req, res) {
 });
 
 app.post("/register", function(req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-
-    newUser.save(function(err){ // when save will encrypt password
-        if(err){
-            console.log(err);
-        } else {
-            res.render("secrets");
-        }
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+    
+        newUser.save(function(err){ // when save will encrypt password
+            if(err){
+                console.log(err);
+            } else {
+                res.render("secrets");
+            }
+        });
     });
 });
 
 app.post("/login", function(req, res) {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
     User.findOne({email: username}, function(err, foundUser) { // when find will decrypt password
         if(err) {
             console.log(err);
         } else {
             if(foundUser) {
-                if(foundUser.password === password) {
-                    res.render("secrets");
-                }
-            }
+                bcrypt.compare(password, foundUser.password, function(err, result) { // to check/compare a password
+                    if (result === true) {
+                        res.render("secrets");
+                    }
+                });
+            } 
         }
     })
 });
